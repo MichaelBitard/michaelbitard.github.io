2d9c23e634d68b74afa16a3357b24040557445bd
Michaël Bitard <bitard.michael@gmail.com>
2013-10-29 23:39:37 +0100

    with opacity it's better

--- scripts/jsIncluder.js
+++ scripts/jsIncluder.js
@@ -1,4 +1,6 @@
 if (document.documentElement.clientWidth >= 640) {
     $.getScript("scripts/jquery.cycle2.js");
     $.getScript("scripts/vcard.js");
+} else {
+    $("#wrapper").animate({opacity: 1}, 1000);
 }
