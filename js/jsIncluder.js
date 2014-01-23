e4e3d1cf6904ccaadde153b6439d7aac00383d39
Michaël Bitard <bitard.michael@gmail.com>
2014-01-23 09:05:12 +0100

    added minify with grunt

--- js/jsIncluder.js
+++ js/jsIncluder.js
@@ -1,6 +1,6 @@
 if (document.documentElement.clientWidth >= 640) {
-    $.getScript("scripts/jquery.cycle2.js");
-    $.getScript("scripts/vcard.js");
+    //$.getScript("scripts/jquery.cycle2.js");
+    $.getScript("min/vcard.js");
 } else {
     $("#wrapper").animate({opacity: 1}, 1000);
 }
