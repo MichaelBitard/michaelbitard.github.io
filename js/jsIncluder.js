eff2517f64d1b31694c7b902c06729f30ec4da88
Bitard Michaël <bitard.michael@gmail.com>
2020-07-25 13:54:02 +0200

    new resume

--- js/jsIncluder.js
+++
@@ -1,6 +0,0 @@
-if (document.documentElement.clientWidth >= 640) {
-    //$.getScript("scripts/jquery.cycle2.js");
-    $.getScript("min/vcard.js");
-} else {
-    $("#wrapper").animate({opacity: 1}, 1000);
-}
--- min/jsIncluder.js
+++
@@ -1,1 +0,0 @@
-document.documentElement.clientWidth>=640?$.getScript("min/vcard.js"):$("#wrapper").animate({opacity:1},1e3);
